713aff246372c395cb205bcc065efe495ad47a20
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-24 08:48:11 -0700

    Add RunningSumOf1dArray.java

---
+++ src/main/java/level1/RunningSumOf1dArray.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package level1;
+
+/**
+ *
+ * @author User
+ */
+public class RunningSumOf1dArray {
+    public int runningSum(int[] nums){
+        for(int i = 1; i < nums.length; i++){
+            nums[i] = nums[i-1]+nums[i];
+        }
+        return nums;
+    }
+}
